423309f3b79a98da586806d4aca372403558c9a4
Luca Capuccini <93596829+cappuu02@users.noreply.github.com>
2023-01-03 21:24:02 +0100

    Add files via upload

---
+++ gamelib.h
@@ -0,0 +1,2 @@
+void combatti();
+void titolo();
---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "gamelib.h"
+
+void quit(){
+    printf("Chitemmuort Frat ro cazz\n");
+    abort();
+}
+
+
+int main(){
+    int a = 0;
+    system("clear");
+    printf("BENVENUTO IN TRIS\n");
+    printf("PREMI ENTER PER CONTINUARE...\n");
+    while((getchar()) != '\n');
+    printf("\n");
+    system("clear");
+    
+    titolo();
+    printf("\n");
+    printf("scegli cosa vuoi fare\n1] Start a Game\n2] Quit Game\n");
+    scanf("%d", &a);
+    switch(a){
+        case 1:
+            combatti();
+        break;
+        case 2:
+            quit();
+        break;
+    }
+    
+    
+    
+}//fine main
